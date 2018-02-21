2082b5d2e6f740939c5ca3e3192e001e0321effe
Muhammad Fahmi <mifah405@gmail.com>
2018-02-21 18:21:34 +0700

    add database

---
+++ db.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.25-MariaDB : Database - db_test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_test`;
+
+/*Table structure for table `tb_user` */
+
+DROP TABLE IF EXISTS `tb_user`;
+
+CREATE TABLE `tb_user` (
+  `pk_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) DEFAULT NULL,
+  `password` char(32) DEFAULT NULL,
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`pk_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_user` */
+
+insert  into `tb_user`(`pk_user`,`username`,`password`,`nama_lengkap`,`email`) values (4,'adaskfn','21232f297a57a5a743894a0e4a801fc3','nvklsdnv','nslkdnv@sdf.sdf'),(6,'asdasd',NULL,'nflsndf','lnvflsw@fsdf.sdf'),(10,'admin','21232f297a57a5a743894a0e4a801fc3','admin','admin@admin.admin'),(11,'asdas','196b0f14eba66e10fba74dbf9e99c22f','asdas','asda@sdfsf.sdfdsf'),(12,'admin','21232f297a57a5a743894a0e4a801fc3','admin','admin@admin.admin');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
